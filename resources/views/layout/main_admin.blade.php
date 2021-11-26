7d559cf9dc558462b8b5c0f940c3234da288184e
enisda-libra <learningenisda@gmail.com>
2021-11-26 18:19:33 +0800

    menambahkan asset untuk admin
    Co-Authored-By: Risdianti <93016894+risdianti@users.noreply.github.com>
    Co-Authored-By: jessikakindly <92999172+jessikakindly@users.noreply.github.com>

---
+++ resources/views/layout/main_admin.blade.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Admin Dashboard</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="admin/img/favicon.png" rel="icon">
+  <link href="admin/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="admin/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="admin/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="admin/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="admin/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="admin/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="admin/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="admin/vendor/simple-datatables/style.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="admin/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  @include('partial.admin.header')
+  <!-- ===== End Header ===== -->
+
+  <!-- ======= Sidebar ======= -->
+  @include('partial.admin.sidebar')
+  <!-- End Sidebar-->
+
+  <main id="main" class="main">
+
+    <div class="pagetitle">
+      <h1>Blank Page</h1>
+      <nav>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="/wp-admin">Home</a></li>
+          <li class="breadcrumb-item">Pages</li>
+          <li class="breadcrumb-item active">Blank</li>
+        </ol>
+      </nav>
+    </div><!-- End Page Title -->
+
+    <section class="section">
+      <div class="row">
+        <div class="col-lg-6">
+
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Example Card</h5>
+              <p>This is an examle page with no contrnt. You can use it as a starter for your custom pages.</p>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="col-lg-6">
+
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Example Card</h5>
+              <p>This is an examle page with no contrnt. You can use it as a starter for your custom pages.</p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  @include('partial.admin.footer')
+  <!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="admin/vendor/apexcharts/apexcharts.min.js"></script>
+  <script src="admin/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="admin/vendor/chart.js/chart.min.js"></script>
+  <script src="admin/vendor/echarts/echarts.min.js"></script>
+  <script src="admin/vendor/quill/quill.min.js"></script>
+  <script src="admin/vendor/simple-datatables/simple-datatables.js"></script>
+  <script src="admin/vendor/tinymce/tinymce.min.js"></script>
+  <script src="admin/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="admin/js/main.js"></script>
+
+</body>
+
+</html>
