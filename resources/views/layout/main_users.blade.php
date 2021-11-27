0ed61245c7cb80fe8d0e650608aa33cbd3a2fc51
enisda-libra <learningenisda@gmail.com>
2021-11-27 08:21:24 +0800

    membuat partial user
    Co-Authored-By: AhmadRafii <95061582+AhmadRafii@users.noreply.github.com>
    Co-Authored-By: Risdianti <93016894+risdianti@users.noreply.github.com>
    Co-Authored-By: jessikakindly <92999172+jessikakindly@users.noreply.github.com>

---
+++ resources/views/layout/main_users.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Home | IEvents</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="users/img/favicon.png" rel="icon">
+  <link href="users/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Montserrat:300,400,500,700" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="users/vendor/aos/aos.css" rel="stylesheet">
+  <link href="users/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="users/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="users/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="users/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="users/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: NewBiz - v4.7.0
+  * Template URL: https://bootstrapmade.com/newbiz-bootstrap-business-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+    @include('partial.users.header')
+  <!-- #header -->
+
+  <!-- ======= Hero Section ======= -->
+    @include('partial.users.hero')
+  <!-- End Hero Section -->
+  
+  <!-- ======= Main ====== -->
+    @include('partial.users.main')
+  <!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  @include('partial.users.footer')
+  <!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="users/vendor/purecounter/purecounter.js"></script>
+  <script src="users/vendor/aos/aos.js"></script>
+  <script src="users/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="users/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="users/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="users/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="users/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="users/js/main.js"></script>
+
+</body>
+
+</html>
