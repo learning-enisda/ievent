785ffbd465e302e8a428ca1738a52429e1ef5140
enisda-libra <learningenisda@gmail.com>
2021-12-01 21:35:19 +0800

    Update hero.blade.php

--- resources/views/partial/users/hero.blade.php
+++ resources/views/partial/users/hero.blade.php
@@ -6,10 +6,10 @@
       </div>
 
       <div class="hero-info" data-aos="zoom-in" data-aos-delay="100">
-        <h2>We provide<br><span>solutions</span><br>for your business!</h2>
+        <h2>Temukan<br><span>Event</span><br>terbaik untukmu</h2>
         <div>
-          <a href="#about" class="btn-get-started scrollto">Get Started</a>
-          <a href="#services" class="btn-services scrollto">Our Services</a>
+          <a href="/post" class="btn-get-started scrollto">Get Started</a>
+          <a href="/login" class="btn-services">Login</a>
         </div>
       </div>
 
